d93a46a07271e478bb3b3df3af880932be5ab48a
AdrianoPereiraRibeiro <adrianoribeiro291005@gmail.com>
2024-03-20 20:53:09 -0300

    Add project files.

---
+++ Diamante/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Diamante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor ímpar para gerar o diamante:"); 
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("O número não é ímpar. Por favor, insira um número ímpar.");
+            }
+            else
+            {
+                int espacos = n / 2;
+                int xxx = 1;
+
+               
+                for (int i = 0; i < (n / 2) + 1; i++)
+                {
+                    Console.Write(new string(' ', espacos));
+                    Console.WriteLine(new string('X', xxx));
+                    espacos--;
+                    xxx += 2;
+                }
+
+                espacos = 1;
+                xxx = n - 2;
+
+                
+                for (int i = (n / 2); i > 0; i--)
+                {
+                    Console.Write(new string(' ', espacos));
+                    Console.WriteLine(new string('X', xxx));
+                    espacos++;
+                    xxx -= 2;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
